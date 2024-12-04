6c20513c55f3e9cd1656e7d1b42fcb21d5e2c34b
sksamiii <128906151+sksamiii@users.noreply.github.com>
2024-12-04 23:46:05 +0530

    Your commit message here

---
+++ frontend/todo-frontend/src/component/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer(){
+    return(
+        
+        <footer className="footer-title footer-font">
+            <p>© 2024 All Rights Reserved.</p>
+        </footer>
+          
+        
+    );
+}
